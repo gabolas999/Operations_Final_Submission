146679cea52f210a641437c5ecdad6071e9f5df9
Theophile <bowmigow@gmail.com>
2026-02-09 13:03:50 +0100

    applied the changes said in the prev commit message

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -3,47 +3,65 @@ from scenarios import SCENARIO_II, SCENARIO_III
 
 from pathlib import Path
 import json
+import copy
 
 from pie_chart import generate_sensitivity_pie_charts
 
 
 VARIABLE_CHANGE_RESULT_SUMMARY_MAP = {
     "gamma": {
-        "0": None,
-        "-35": None,
-        "-25": None,
-        "-15": None,
-        "15": None,
-        "25": None,
-        "35": None,
+        "-60": None,
+        "-40": None,
+        "-20": None,
+        "20": None,
+        "40": None,
+        "60": None,
     },
     "h_t": {
-        "0": None,
-        "-35": None,
-        "-25": None,
-        "-15": None,
-        "15": None,
-        "25": None,
-        "35": None,
+        "-60": None,
+        "-40": None,
+        "-20": None,
+        "20": None,
+        "40": None,
+        "60": None,
     },
     "handling_time": {
-        "0": None,
-        "-35": None,
-        "-25": None,
-        "-15": None,
-        "15": None,
-        "25": None,
-        "35": None,
+        "-60": None,
+        "-40": None,
+        "-20": None,
+        "20": None,
+        "40": None,
+        "60": None,
     },
 }
 
 
 def run_sensitivity_analysis(
-    base_scenario=SCENARIO_III,
+    base_scenario=SCENARIO_II,
     output_path=Path(
         "./Storage/theo_results/sensitivity_analysis/sensitivity_analysis_results.json"
     ),
 ):
+
+    _, _, result_dict, _ = main(
+        input_scenario_dict=base_scenario,
+        scenario_name=f"Base_Scenario",
+        max_iters=10000,
+    )
+
+    result_summary = result_dict["summary"]
+
+    result_summary["barge_share_%"] = (
+        result_summary["containers_on_barges"] / result_summary["total_containers"]
+    ) * 100
+    result_summary["truck_share_%"] = (
+        result_summary["containers_trucked"] / result_summary["total_containers"]
+    ) * 100
+
+    baseline_result_dict = result_dict
+    baseline_barge_share = result_summary["barge_share_%"]
+    baseline_truck_share = result_summary["truck_share_%"]
+    baseline_total_cost = result_summary["final_cost"]
 
     for variable in VARIABLE_CHANGE_RESULT_SUMMARY_MAP.keys():
         baseline_result_dict = None
@@ -55,7 +73,8 @@ def run_sensitivity_analysis(
             variable
         ].keys():
             change_percentage = int(change_percentage_str)
-            modified_scenario = base_scenario.copy()
+            modified_scenario = {}
+            modified_scenario = copy.deepcopy(base_scenario)
 
             if variable == "gamma":
                 assert "gamma" in modified_scenario.keys()
@@ -86,27 +105,20 @@ def run_sensitivity_analysis(
                 / result_summary["total_containers"]
             ) * 100
 
-            if change_percentage == 0:
-                baseline_result_dict = result_dict
-                baseline_barge_share = result_summary["barge_share_%"]
-                baseline_truck_share = result_summary["truck_share_%"]
-                baseline_total_cost = result_summary["final_cost"]
-            else:
+            # pp = percentage points
+            result_summary["barge_share_change_pp"] = (
+                result_summary["barge_share_%"] - baseline_barge_share
+            )
+            result_summary["truck_share_change_pp"] = (
+                result_summary["truck_share_%"] - baseline_truck_share
+            )
 
-                # pp = percentage points
-                result_summary["barge_share_change_pp"] = (
-                    result_summary["barge_share_%"] - baseline_barge_share
-                )
-                result_summary["truck_share_change_pp"] = (
-                    result_summary["truck_share_%"] - baseline_truck_share
-                )
-
-                # percentage change (+ means increase, - means decrease)
-                result_summary["total_cost_change_%"] = (
-                    (result_summary["final_cost"] - baseline_total_cost)
-                    / baseline_total_cost
-                    * 100
-                )
+            # percentage change (+ means increase, - means decrease)
+            result_summary["total_cost_change_%"] = (
+                (result_summary["final_cost"] - baseline_total_cost)
+                / baseline_total_cost
+                * 100
+            )
 
             # print(result_summary)
 
@@ -140,7 +152,7 @@ def analyse_sensitivity_analysis_results(
 
     generate_sensitivity_pie_charts(
         results=results_data,
-        n_rows=7,
+        n_rows=6,
         n_cols=3,
         output_path="./Storage/theo_results/sensitivity_analysis/sensitivity_pies.png",
         dpi=1200,
